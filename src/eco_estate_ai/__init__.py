aa556be76597a1e89db5251ceeb41fd4af93000b
Oleksandr Brusyltsev <Brusiltsev1@gmail.com>
2025-03-23 13:24:52 +0200

    optimize import

--- src/eco_estate_ai/__init__.py
+++ src/eco_estate_ai/__init__.py
@@ -2,16 +2,12 @@ from .assistant import EcoEstateAssistant
 from .embedder import Embedder
 from .storage import QAStorage
 from .detector import LanguageDetector
-from ..utils.const import Const
+from .factory import create_assistant
 
-QA_PAIRS_PATH = "src/data/qa_pairs.json"
-MODEL_PATH = "src/eco_estate_ai/models/local_model"
-
-def create_assistant(
-    qa_path: str = Const.QA_PAIRS_PATH,
-    model_path: str = Const.MODEL_PATH
-) -> EcoEstateAssistant:
-    _storage = QAStorage(qa_path)
-    _embedder = Embedder(model_path)
-    _detector = LanguageDetector()
-    return EcoEstateAssistant(_storage, _embedder, _detector)
+__all__ = [
+    "EcoEstateAssistant",
+    "Embedder",
+    "QAStorage",
+    "LanguageDetector",
+    "create_assistant",
+]
---
+++ src/eco_estate_ai/factory.py
@@ -0,0 +1,12 @@
+from src.eco_estate_ai import EcoEstateAssistant, Embedder, LanguageDetector, QAStorage
+from src.utils.const import Const
+
+
+def create_assistant(
+    qa_path: str = Const.QA_PAIRS_PATH,
+    model_path: str = Const.MODEL_PATH
+) -> EcoEstateAssistant:
+    _storage = QAStorage(qa_path)
+    _embedder = Embedder(model_path)
+    _detector = LanguageDetector()
+    return EcoEstateAssistant(_storage, _embedder, _detector)

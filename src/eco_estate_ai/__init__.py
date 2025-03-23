047266c5855e6b931448ec24aa23c1a91ea9ac66
Oleksandr Brusyltsev <Brusiltsev1@gmail.com>
2025-03-23 13:40:11 +0200

    optimize import x2

--- src/eco_estate_ai/__init__.py
+++ src/eco_estate_ai/__init__.py
@@ -1,8 +1,8 @@
-from src.eco_estate_ai.assistant import EcoEstateAssistant
-from src.eco_estate_ai.embedder import Embedder
-from src.eco_estate_ai.storage import QAStorage
-from src.eco_estate_ai.detector import LanguageDetector
-from src.eco_estate_ai.factory import create_assistant
+from .assistant import EcoEstateAssistant
+from .embedder import Embedder
+from .storage import QAStorage
+from .detector import LanguageDetector
+from .factory import create_assistant
 
 __all__ = [
     "EcoEstateAssistant",

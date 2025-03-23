d4fa57ebd1e5140fcd6db6041e9a07d3f942b72d
Oleksandr Brusyltsev <Brusiltsev1@gmail.com>
2025-03-23 12:47:54 +0200

    minor improve toml

--- src/tests/test_assistant.py
+++ src/tests/test_assistant.py
@@ -1,11 +1,11 @@
-from eco_estate_ai import (EcoEstateAssistant, Embedder, LanguageDetector,
-                           QAStorage)
+from src.eco_estate_ai import QAStorage, Embedder, LanguageDetector, EcoEstateAssistant
+from src.utils.const import Const
 
 
 def test_answer():
-    storage = QAStorage("data/qa_pairs.json")
-    embedder = Embedder("models/multilingual_model")
+    storage = QAStorage("src/data/qa_pairs.json")
+    embedder = Embedder(Const.MODEL_PATH)
     detector = LanguageDetector()
     assistant = EcoEstateAssistant(storage, embedder, detector)
 
-    assert "Олешня" in assistant.get_answer("где находится усадьба").lower()
+    assert "Олешня" in assistant.get_answer("где находится усадьба")

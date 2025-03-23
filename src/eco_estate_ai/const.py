d965d6d6234b03685c231a3eeb5d63d022b04f6f
Oleksandr Brusyltsev <Brusiltsev1@gmail.com>
2025-03-23 14:21:09 +0200

    fix path

--- src/eco_estate_ai/const.py
+++ src/eco_estate_ai/const.py
@@ -3,5 +3,5 @@ class Const:
     RU = "ru"  # Russian
     DEFAULT_ANSWER_RU = "Извините, я пока не знаю ответа на этот вопрос 😔"
     DEFAULT_ANSWER_UK = "Вибачте, я поки не знаю відповіді на це питання 😔"
-    QA_PAIRS_PATH = "src/data/qa_pairs.json"
+    QA_PAIRS_PATH = "data/qa_pairs.json"
     MODEL_PATH = "paraphrase-multilingual-MiniLM-L12-v2"

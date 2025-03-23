547c5e5c1ce0f7f01fea43a40bd3820f8a2025fb
Oleksandr Brusyltsev <Brusiltsev1@gmail.com>
2025-03-23 13:16:22 +0200

    change models

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
-# from setuptools import find_packages, setup
-#
-# setup(
-#     name="eco_estate_ai_assistant",
-#     version="0.1.0",
-#     packages=find_packages(),
-#     install_requires=["sentence-transformers", "langdetect", "torch"],
-#     description="Multilingual AI assistant for Eco Estate Telegram bots",
-#     author="Oleksandr Brusyltsev",
-#     url="https://github.com/OleksandrBrusyltsev/eco-estate-ai-assistant",
-#     license="MIT",
-# )
-#
-from src.eco_estate_ai import create_assistant
+from setuptools import find_packages, setup
 
-# from eco_estate_ai import EcoEstateAssistant, QAStorage, Embedder, LanguageDetector
-#
-# storage = QAStorage("data/qa_pairs.json")
-# embedder = Embedder("models/multilingual_model")
-# detector = LanguageDetector()
-# assistant = EcoEstateAssistant(storage, embedder, detector)
-#
-# print(assistant.get_answer("где находится усадьба"))
-assistant = create_assistant()
-print(assistant.get_answer("как добраться до усдаьбы"))
+setup(
+    name="eco_estate_ai_assistant",
+    version="0.1.0",
+    packages=find_packages(where="eco-estate-ai-assistant/src/eco_estate_ai/"),
+    install_requires=["sentence-transformers", "langdetect", "torch"],
+    description="Multilingual AI assistant for Eco Estate Telegram bots",
+    author="Oleksandr Brusyltsev",
+    url="https://github.com/OleksandrBrusyltsev/eco-estate-ai-assistant",
+    license="MIT",
+)
--- src/eco_estate_ai/embedder.py
+++ src/eco_estate_ai/embedder.py
@@ -2,9 +2,8 @@ import time
 
 from sentence_transformers import SentenceTransformer
 
-
 class Embedder:
-    def __init__(self, model_path: str = "models/local_model"):
+    def __init__(self, model_path: str = "sentence-transformers/gtr-t5-base"):
         self.model = SentenceTransformer(model_path)
 
     def encode(self, texts: list[str]):
